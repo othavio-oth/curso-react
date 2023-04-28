29a515db6c62648c03c8e6de7400fafcdcc5b005
ooda <othavio.sousa@gft.com>
2023-04-28 11:18:45 -0300

    implement notFound component

--- navigation/src/components/layout/Content.jsx
+++ navigation/src/components/layout/Content.jsx
@@ -4,13 +4,15 @@ import { Routes, Route } from "react-router-dom";
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 const Content = () => {
   return (
     <main className="content">
       <Routes>
         <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
         <Route path="/param/:id" element={<Param />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   );
