366bb0985ac81c139f0362495fb966fe51de0437
ooda <othavio.sousa@gft.com>
2023-05-01 16:50:49 -0300

    change useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function getFatorial(number) {
+  if (number <= 0) return -1;
+  if (number === 1) return 1;
+  return getFatorial(number - 1) * number;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">100</span>
+        </div>
+      </div>
+      <input
+        type="number"
+        className="input"
+        value={number}
+        onChange={(event) => setNumber(event.target.value)}
+      />
+      <button onClick={() => getFatorial(parseInt(number))}>
+        Get Fatorial
+      </button>
+    </div>
+  );
+};
+
+export default UseEffect;
