4b6423c7f5c722ab9ded8434f5e759373e69e1a0
ooda <othavio.sousa@gft.com>
2023-05-04 14:21:55 -0300

    add next project

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
+  const [value1, setValue1] = useState("5");
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercicio 01" />
+      <div className="center">
+        <div>
+          <span className="text">Valor: </span>
+          <span className="text">{value1} [</span>
+          <span className="text red">Contador</span>
+          <span className="text">]</span>
         </div>
-    )
-}
+        <input type="text" className="input" />
+      </div>
+    </div>
+  );
+};
 
-export default UseRef
+export default UseRef;
